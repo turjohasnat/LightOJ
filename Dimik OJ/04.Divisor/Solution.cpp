17cccbac4325643e35c0b3ea9844c3a389f65511
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-02-15 08:48:38 +0600

    Create Solution.cpp

---
+++ Dimik OJ/04.Divisor/Solution.cpp
@@ -0,0 +1,129 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define all(a) (a).begin(),(a).end()
+#define sz(x) (int)x.size()
+#define mid(l,r) ((r+l)/2)
+#define left(node) (node*2)
+#define right(node) (node*2+1)
+#define mx_int_prime 999999937
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+            return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+            os << "{";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const multiset< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << it -> first << " = " << it -> second ;
+                                            }
+                    return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+void faltu () {
+            cerr << endl;
+}
+
+template <typename T>
+void faltu( T a[], int n ) {
+            for(int i = 0; i < n; ++i) cerr << a[i] << ' ';
+                cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) {
+            cerr << arg << ' ';
+                faltu(rest...);
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    for(int i = 1; i <= t; i++){
+        ll n;
+        cin >> n;
+
+        cout << "Case " << i << ":";
+        for(int j = 1; j <= n; j++){
+            if(n % j == 0)
+                cout << " " << j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
